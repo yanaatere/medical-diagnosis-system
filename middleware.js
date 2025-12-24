dc9e274f9c0368205b31700ee4c75a880a3eecd7
Yana Andika <yana.andik@netzme.id>
2025-12-25 05:44:22 +0700

    3 commit

--- middleware.js
+++ middleware.js
@@ -28,7 +28,9 @@ export async function middleware(request) {
     return NextResponse.next();
   } catch (error) {
     // Invalid token, redirect to login
-    return NextResponse.redirect(new URL('/login', request.url));
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.delete('token');
+    return response;
   }
 }
 
